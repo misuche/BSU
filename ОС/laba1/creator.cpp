9c8dfb320d757effe116edceb327ad8a67d5ab47
misuche <lmisuchenko@gmail.com>
2022-05-08 13:22:37 +0300

    some changes

--- ОС/laba1/creator.cpp
+++ ОС/laba1/creator.cpp
@@ -2,21 +2,22 @@
 #include <fstream>
 #include <sstream>
 #include "employee.h"
-using namespace std;
-int main(int argc, char* argv[]) {
-    fstream out(argv[0], ios::out | ios::binary);//создаем бинарный файл
-    istringstream is(argv[1]);
+
+
+int main(int argc, char *argv[]) {
+    std::fstream out(argv[0], std::ios::out | std::ios::binary);
+    std::istringstream is(argv[1]);
     int amount;
-    is>>amount;
+    is >> amount;
     for (int i = 0; i < amount; ++i) {
         employee e;
-        cout<<"enter id\n";
-        cin>>e.num;
-        cout<<"enter name\n";
-        cin>>e.name;
-        cout<<"enter hours\n";
-        cin>>e.hours;
-        out.write((char*)&e,sizeof(employee));
+        std::cout << "enter id\n";
+        std::cin >> e.num;
+        std::cout << "enter name\n";
+        std::cin >> e.name;
+        std::cout << "enter hours\n";
+        std::cin >> e.hours;
+        out.write((char *) &e, sizeof(employee));
     }
     out.close();
     return 0;
