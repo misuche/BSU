6fbb03f916b86fae8ab899e8577432d43b327861
misuche <lmisuchenko@gmail.com>
2022-05-08 13:13:17 +0300

    some changes

--- ОС/laba1/main.cpp
+++ ОС/laba1/main.cpp
@@ -1,69 +1,82 @@
 #include <iostream>
-#include <string>
-#include <windows.h>
-#include <stdio.h>
 #include <tchar.h>
 #include <sstream>
 #include <fstream>
+#include <windows.h>
 #include "employee.h"
 
-using namespace std;
-void startCreator(string binFileName, int amount)
-{
+
+void startCreator(std::string &binFileName, int amount) {
     STARTUPINFO si;
     PROCESS_INFORMATION pi;
+
     ZeroMemory(&si, sizeof(si));
     si.cb = sizeof(si);
     ZeroMemory(&pi, sizeof(pi));
-    const char* app_name = "Creator.exe";
-    stringstream ss;
-    ss<<amount;
-    string arguments = binFileName + " " + ss.str();
-    CreateProcess("Creator.exe", (char*)arguments.c_str(), NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi);
+
+    std::stringstream ss;
+    ss << amount;
+    std::string arguments = binFileName + ' ' + ss.str();
+
+    bool flag = CreateProcess("Creator.exe", (char *) arguments.c_str(), NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL,
+                              NULL, &si, &pi);
+    if (!flag)
+        GetLastError();
     WaitForSingleObject(pi.hProcess, INFINITE);
     CloseHandle(pi.hThread);
     CloseHandle(pi.hProcess);
 }
-void startReporter(string binFileName, string reportFileName, double salary)
-{
+
+void startReporter(std::string &binFileName, std::string &reportFileName, double salary) {
     STARTUPINFO si;
     PROCESS_INFORMATION pi;
+
     ZeroMemory(&si, sizeof(si));
     si.cb = sizeof(si);
     ZeroMemory(&pi, sizeof(pi));
-    const char* app_name = "Reporter.exe";
-    stringstream ss;
-    ss<<salary;
-    string arguments = binFileName + " " +reportFileName+" " + ss.str();
-    CreateProcess("Reporter.exe", (char*)arguments.c_str(), NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi);
+
+    std::stringstream ss;
+    ss << salary;
+    std::string arguments = binFileName + " " + reportFileName + " " + ss.str();
+    bool flag = CreateProcess("Reporter.exe", (char *) arguments.c_str(), NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL,
+                              NULL, &si,
+                              &pi);
+    if (!flag) GetLastError();
     WaitForSingleObject(pi.hProcess, INFINITE);
     CloseHandle(pi.hThread);
     CloseHandle(pi.hProcess);
 
 }
+
 int main() {
-    string binFileName; int amount;
-    cout << "enter binary file name\n";
-    cin >> binFileName;
-    cout << "enter amount of employees\n";
-    cin >> amount;
-    startCreator(binFileName,amount);
-    ifstream in(binFileName.c_str(), ios::binary);
+    std::string binFileName;
+    int amount;
+    std::cout << "enter binary file name\n";
+    std::cin >> binFileName;
+    std::cout << "enter amount of employees\n";
+    std::cin >> amount;
+
+    startCreator(binFileName, amount);
+
+    std::ifstream in(binFileName.c_str(), std::ios::binary);
     employee e;
-    while (in.read((char*)&e, sizeof(employee))) {
-        cout << e.num << " " << e.name << " " << e.hours << endl;
+    while (in.read((char *) &e, sizeof(employee))) {
+        std::cout << e.num << " " << e.name << " " << e.hours << std::endl;
     }
+
     in.close();
-    string reportFileName; int salary;
-    cout<<"enter report file name\n";
-    cin>>reportFileName;
-    cout<<"enter salary\n";
-    cin>>salary;
+
+    std::string reportFileName;
+    double salary;
+    std::cout << "enter report file name\n";
+    std::cin >> reportFileName;
+    std::cout << "enter salary\n";
+    std::cin >> salary;
     startReporter(binFileName, reportFileName, salary);
-    string line;
+    std::string line;
     in.open(reportFileName.c_str());
-    while(getline(in, line))
-        cout<<line<<endl;
+    while (std::getline(in, line))
+        std::cout << line << std::endl;
     system("pause");
     return 0;
 }
